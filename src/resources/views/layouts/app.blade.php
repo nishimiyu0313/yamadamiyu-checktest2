cb0cdeffee532e949232f9b8734509dae606c3ad
miyu <yookmiyu@kvision.ne.jp>
2025-02-23 12:41:42 +0900

    リポジトリの変更

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mogitate</title>
     <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
     @yield('css')
 </head>
 
